62c700b035f687cf32e53b0a1091d5a13198ae5a
I Putu Haris Setiadi Ekatama <69114510+harisekatama@users.noreply.github.com>
2023-12-10 20:41:00 +0700

    feat: add auto sending messages

--- main/TestConnection/SendBluetoothJSON.py
+++ main/TestConnection/SendBluetoothJSON.py
@@ -1,10 +1,13 @@
-import bluetooth
 import json
+import bluetooth
+import datetime
+import random
+import time
 
 # ESP32 Bluetooth address
-esp32_address = "EC:62:60:9B:E4:92"  # Replace with your ESP32's Bluetooth address
+# esp32_address = "EC:62:60:9B:E4:92"  # Replace with your ESP32's Bluetooth address
 
-# esp32_address = "30:C6:F7:2F:31:86" # Iqbal's ESP32
+esp32_address = "C0:49:EF:E7:BD:EA"
 
 # Create a Bluetooth socket
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -12,22 +15,28 @@ sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 # Connect to the ESP32
 sock.connect((esp32_address, 1))
 
+i = 0
+
 while True:
-    # Get keyboard input for two values
-    arah = input("Masukkan Arah: ")
-    kecepatan = input("Masukkan Kecepatan: ")
+    if i < 6:
+        # Get keyboard input for two values
+        arah = random.choice('ABCDE')
+        kecepatan = random.randint(0,255)
     
-    # Create a JSON object
-    json_data = {"arah": arah, "kecepatan": kecepatan}
+        # Create a JSON object
+        json_data = {"arah": arah, "kecepatan": kecepatan}
+        print(json_data)
 
-    # Serialize the JSON data
-    json_string = json.dumps(json_data)
-    
-    if arah == "q" or kecepatan == "q":
-        break
-
-    # Send the serialized JSON over Bluetooth
-    sock.send(json_string)
+        # Serialize the JSON data
+        json_string = json.dumps(json_data)
+        
+        sock.send(json_string)
+        
+        i = i + 1
+        time.sleep(1.1)
+        
+        if i == 6:
+            break
 
 # Close the Bluetooth socket
 sock.close()
